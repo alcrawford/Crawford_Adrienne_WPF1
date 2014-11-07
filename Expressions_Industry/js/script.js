39d6ea36a19d4d4bd98e025bc96309fbf8052bf0
alcrawford <adriennelorton@hotmail.com>
2014-11-06 22:28:23 -0500

    Expressions Assignment

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,4 @@
 //Adrienne Crawford, November 6, 2014 Industry Expressions
 
 var userInput = prompt ("As a Web Development major, how much did you spend last year on tuition?");
+console.log(userInput)
