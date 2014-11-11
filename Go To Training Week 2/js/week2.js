89231ca7c6f575620d9e058a2f86c94b80c0e390
alcrawford <adriennelorton@hotmail.com>
2014-11-11 13:22:44 -0500

    Go To Training Week 2

--- Go To Training Week 2/js/week2.js
+++ Go To Training Week 2/js/week2.js
@@ -4,3 +4,12 @@ WPF Week 2 Training
  */
 
 alert ("Testing 1,2,3");
+
+//Create a js file that will tell us how old we are
+//Ask the user what year they were born
+//Create a variable to catch their answer
+
+var yearBorn = prompt ("What year were you born");
+
+//console log out the response to check
+console.log(yearBorn);
