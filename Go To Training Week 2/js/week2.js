75bd67b7152878e5389031b0c1af0b8a553fa81f
alcrawford <adriennelorton@hotmail.com>
2014-11-11 16:03:46 -0500

    Prompt name and year born
    Prompt name and year born

--- Go To Training Week 2/js/week2.js
+++ Go To Training Week 2/js/week2.js
@@ -3,7 +3,16 @@
 WPF Week 2 Training
  */
 
-alert ("Testing 1,2,3");
+//alert ("Testing 1,2,3");
+
+//Ask the user their name
+
+var name = prompt("Please type in your name");
+
+//Say hello to the user and let them know what we are doing
+//Alert the user
+
+alert("Welcome "+name+"! Let's go ahead and figure out how old you are");
 
 //Create a js file that will tell us how old we are
 //Ask the user what year they were born
