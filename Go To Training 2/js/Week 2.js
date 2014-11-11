bf1a00d3af8615656834dca14e14d0aeeba2dd49
alcrawford <adriennelorton@hotmail.com>
2014-11-11 12:58:44 -0500

    Go To Training Week 2
     Practice Week 2

--- Go To Training 2/js/Week 2.js
+++
@@ -1,7 +0,0 @@
-/* Adrienne Crawford
-WPF Section 01
-November 6, 2014
-Go To Training 2
- */
-/*alert("this be me, 2310");
-its a glorious success*/
---
+++ Go To Training Week 2/js/week2.js
@@ -0,0 +1,6 @@
+/*Adrienne Crawford
+11/11/14
+WPF Week 2 Training
+ */
+
+alert ("Testing 1,2,3");
