e80f10c9e3792a961440b472f1f0913236685afd
alcrawford <adriennelorton@hotmail.com>
2014-11-12 08:16:16 -0500

    industry variables complete
    industry variables complete

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -20,3 +20,16 @@ var incomeTax = prompt ("In numbers only, tell me how many dollars you payed in 
 //I set up the last variable return.
 console. log(incomeTax);
 //console log returns
+
+var yearSum = Number(freeLance) + Number(jobSalary);
+//I created a variable to add the figures with concantonation
+console. log(yearSum);
+//console log returns
+
+var yearIncome = (yearSum - incomeTax);
+//I created a variable to subtract the income tax and give us a final figure
+console.log(yearIncome);
+//console log returns
+
+alert("Last year, you made approximately $"+yearIncome+" as a Web Developer. Thank you for helping.");
+//I created an alert to return the figure to the user. Checks.
