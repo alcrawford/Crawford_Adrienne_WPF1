45074782455522e20bea4035f93b6907e19e0b91
alcrawford <adriennelorton@hotmail.com>
2014-11-13 12:00:10 -0500

    hours at light wacky expressions

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,6 +4,14 @@ WPF Week 2 Assignment
  */
 
 
+alert("Welcome to silly trivia quiz! I am going to ask you some odd questions and you can take a good guess!");
 //Alerting user to purpose.
+
+var hoursLight = prompt("How many hours do you think the average person spends in their lifetime waiting for the light tp turn from red to green?");
+log.console(hoursLight);
+
+
+
+
 
 
