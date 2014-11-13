c12d94d2037c6266a4bcf5e7fe1e5ee161f95e00
alcrawford <adriennelorton@hotmail.com>
2014-11-12 19:45:38 -0500

    personal costSmoke, Industry done

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,10 +4,16 @@
  */
 
 alert("I quit smoking. Sometimes I think about all the damage it caused, and the money it cost me.");
-//First I prepare reader for purpose. Then check in console.
+//First I prepare reader for purpose. Then check in console. Checks.
 
+prompt("Can you guess how much it cost me to smoke a half of a pack of cigarettes a day for 15 years?");
+console.log(prompt);
 
-//var costSmoke = (15 * 958);
-//cost to smoke a half pack a day for a year at 5.25 a pack
+var costSmoke = (15 * 958);
+console.log(costSmoke);
+//cost to smoke a half pack a day for a year at 5.25 a pack multiplied by number of smoking years.
+
+alert("Over the course of fifteen years, I spent over $"+costSmoke+" on cigarettes. Wow!");
+//
 
 
