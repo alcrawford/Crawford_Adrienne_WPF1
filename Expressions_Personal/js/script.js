a9073990d22d810986a9a7ef3cf5abcea523cc1c
alcrawford <adriennelorton@hotmail.com>
2014-11-13 13:12:54 -0500

    cigGuess and costGuess added

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,24 +6,33 @@
 alert("I quit smoking. Sometimes I think about all the damage it caused, and the money it cost me.");
 //First I prepare reader for purpose. Then check in console. Checks.
 
-prompt("Can you guess how much it cost me to smoke a half of a pack of cigarettes a day for 15 years?");
-console.log(prompt);
-//Prompt user to interact.
+var costGuess = prompt("Can you guess how much it cost me to smoke a half of a pack of cigarettes a day for 15 years?");
+console.log(costGuess);
+//Created a variable to get user input
 
 var costSmoke = (15 * 958);
 console.log(costSmoke);
 //Variable created to calculate cost to smoke a half pack a day for a year (10 * 365) / 20= 182.5 packs at 5.25 a pack
 // multiplied by number of smoking years. I would have included the whole math problem, but I didn't know enough.
 
-alert("Actually, over the course of fifteen years, I spent over $"+costSmoke+" on cigarettes. Wow!");
+var costDiff = (costSmoke - costGuess);
+console.log(costDiff);
+//I created a second variable to show the user how close their guess was.
+
+alert("Actually, over the course of fifteen years, I spent over $"+costSmoke+" on cigarettes.You were off by about $"+costDiff+". Wow!");
 //I created an alert to return the real figure to the user. Checks.
 
-prompt("Can you guess approximately how many cigarettes I smoked in that time?");
-//Prompt user to interact.
+var cigGuess = prompt("Can you guess approximately how many cigarettes I smoked in that time?");
+console.log(cigGuess);
+//Created a variable to get user input.
 
 var numberCigs = (365 * 15 * 10);
 console.log(numberCigs);
 //Variable to calculate number of cig's smoked over 15 years.
+
+var cigDiff = (numberCigs - cigGuess);
+console.log(cigDiff);
+//I created a second variable to show the user how close their guess was.
 
 alert("Actually, over the course of fifteen years, PREPARE..., I smoked, "+numberCigs+" cigarettes. Gross.");
 //I created an alert to return the real figure to the user. Checks.
