3c50182b7f051fd1d89da08437bb346e82c888e7
alcrawford <adriennelorton@hotmail.com>
2014-11-13 12:13:55 -0500

    hours off complete wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -9,9 +9,14 @@ alert("Welcome to silly trivia quiz! I am going to ask you some odd questions an
 
 var hoursLight = prompt("How many hours do you think the average person spends, in their lifetime, waiting for the light to turn from red to green?");
 console.log(hoursLight);
+//variable to find out users guess.
 
+var hoursOff = (336 - hoursLight);
+console.log(hoursOff);
+//Variable to find out how far off the user is so it can be returned.
 
-
+alert("Well, you are off by about "+hoursOff+" hours. The answer is 336 hours, or approximately 2 weeks.");
+//Return the result to user
 
 
 
