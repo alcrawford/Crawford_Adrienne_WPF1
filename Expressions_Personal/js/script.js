d8cfbf0f0e77f93597e72150723c07b5f3626faf
alcrawford <adriennelorton@hotmail.com>
2014-11-12 19:35:39 -0500

    alert expressions personal, industry done

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,3 +2,12 @@
  Expressions Personal
  WPF Week 2 Assignment
  */
+
+alert("I quit smoking. Sometimes I think about all the damage it caused, and the money it cost me.");
+//First I prepare reader for purpose. Then check in console.
+
+
+//var costSmoke = (15 * 958);
+//cost to smoke a half pack a day for a year at 5.25 a pack
+
+
