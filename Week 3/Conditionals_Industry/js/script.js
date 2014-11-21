0333fcf9fbcad57e482e861342731b2416f47d9b
alcrawford <adriennelorton@hotmail.com>
2014-11-21 17:34:35 -0500

    5:34 Industry
    5:34 Industry

--- Week 3/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals_Industry/js/script.js
@@ -7,12 +7,22 @@ console.log(codeLang);
 //array created and returns
 
 // In this example, I want to know how many developers are using HTML. So, I create a variable with an if else
-// statement that adds one for every instance of HTML in the array.
+// statement that adds 1 for every instance of HTML in the array.
 
 var numHtml = 0;
 
 if (codeLang[0] === "HTML"){
   numHtml ++;
+}else{
+ console.log("this code is not HTML.");
+}
+if (codeLang[1] === "HTML"){
+ numHtml ++;
+}else{
+ console.log("this code is not HTML.");
+}
+if (codeLang[2] === "HTML"){
+ numHtml ++;
 }else{
  console.log("this code is not HTML.");
 }
