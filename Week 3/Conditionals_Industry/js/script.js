9e86e76bcc5c0663db74737dffde13b8a9e843cc
alcrawford <adriennelorton@hotmail.com>
2014-11-21 17:10:33 -0500

    5:09 Industry
    5:09 Industry

--- Week 3/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals_Industry/js/script.js
@@ -1,6 +1,20 @@
 /*Adrienne Crawford
- Working Conditionals Wacky
+ Working Conditionals Industry
  WPF Week 3 Assignment*/
 
-var codeLang = ["JS","HTML","RUBY","PHP"]
+var codeLang = ["JS","HTML","RUBY","PHP","HTML"]
 console.log(codeLang);
+//array created and returns
+
+// In this example, I want to know how many developers are using HTML. So, I create a variable with an if else
+// statement that adds one for every instance of HTML in the array.
+
+var numHtml = 0;
+
+if (codeLang[0] === "HTML"){
+  numHtml ++;
+}else{
+ console.log("this code is not HTML.");
+}
+
+
