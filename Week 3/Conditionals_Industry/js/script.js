5b5334e33c3ee3f3432cdf4455221dadd35c6acf
alcrawford <adriennelorton@hotmail.com>
2014-11-21 19:14:53 -0500

    7:14 Industry
    7:14 Industry

--- Week 3/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals_Industry/js/script.js
@@ -6,8 +6,8 @@ var codeLang = ["JS","HTML","RUBY","PHP","HTML"]
 console.log(codeLang);
 //array created and returns
 
-// In this example, I want to know how many projects require HTML. So, I create a variable with an if else
-// statement that adds 1 for every instance of HTML in the array.
+// In this example, I am pretending as if I am a supervisor trying to collect data on a web development job. I want to know how many projects
+// require HTML. So, I create a variable with if else statements that add 1 for every instance of HTML in the array.
 
 var numHtml = 0;
 
@@ -38,3 +38,15 @@ if (codeLang[4] === "HTML"){
 }
 
 console.log(numHtml);
+//the final return gives the correct number.
+
+//Next, I prompt the user(employee) for the number of developers needed for each job.
+var workersHtml = prompt("I am going to budget the HTML Project based on a few criteria." +
+"How many developers do you need to complete each HTML project?");
+console.log(workersHtml);
+//returns
+//Next I prompt for the time required to complete the job.
+
+var hoursHtml = prompt("With a full team, how many hours will the project require for completion");
+
+var budgetHtml = (4800 - (workersHtml*hoursHtml));
