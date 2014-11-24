6fb44ce2be7f34d7b9036b8f78ff40d823c3438e
alcrawford <adriennelorton@hotmail.com>
2014-11-24 09:13:10 -0500

    working
    working

--- Week 3/Conditionals_Wacky/js/script.js
+++ Week 3/Conditionals_Wacky/js/script.js
@@ -1,3 +1,25 @@
 /*Adrienne Crawford
  Working Conditionals Wacky
  WPF Week 3 Assignment*/
+
+//setting up variable and prompt to get user input. checks.
+var temp =prompt("The meteorologist is notoriously wrong. So why don't you help me decide what the weather will be like today!" +
+"What do you think the temperature will be today?");
+  console.log(temp);
+
+//set up a series of else if statements to decide what to do based on the users input.
+
+if(temp <= 30){
+   console.log("We will watch movies today!");
+}else if(temp > 31 && temp < 40){
+   console.log("We will go to a movie today!");
+}else if(temp > 41 && temp < 50){
+    console.log("We will go to the market today!");
+}else if(temp > 50 && temp < 60){
+    console.log("We will go to the park today!");
+}else{
+    console.log("Looks like a record breaker to me.");
+}
+
+var actualTemp = prompt("What was the actual temperature today?");
+console.log(actualTemp);
