c6e3a3a116b0b37e55e032feb34a8ed27ad9bc32
alcrawford <adriennelorton@hotmail.com>
2014-11-23 23:23:31 -0500

    draft final
    draft final

--- Week 3/Conditionals_Personal/js/script.js
+++ Week 3/Conditionals_Personal/js/script.js
@@ -5,39 +5,29 @@
 
 
 
-
+//freelance versus corporate
+//prompting potential employer for income.checks
 var freeLanceOffer = prompt("How much can you pay me as a contractor for a year of freelance work?");
     console.log(freeLanceOffer);
 
+//prompting husband (haha) for advice.checks
 var husbSays = prompt("Dear Husband, what is the minimum I can afford to bring home next year?");
     console.log(husbSays);
 
-
+//created a ternary statement to return results
 (freeLanceOffer >= husbSays) ? console.log("I can freelance!") : console.log("This looks corporate.");
 
 
 //Lets assume that my above return was freelance. I use two different logical operators within an Else If statement below
-// to determine if I will need to work at home or if I can afford a nanny.
-var nannyCost = 7900;
+// to determine if I will need to work at home or if I can afford a nanny.checks.
+var nannyCost = 9600;
 
-if(freeLanceOffer >= 50000 && nannyCost <= 9600){
+if(freeLanceOffer >= 50000 && nannyCost <= 7600){                      //logical and operator
    console.log("You can work outside home");
-}else if(freeLanceOffer >= 46000 || nannyCost >= 5600){
+}else if(freeLanceOffer >= 46000 || nannyCost >= 5600){                //logical or operator
    console.log("You can work home part-time");
 }else{
    console.log("You are working at home with kids.");
 }
 
 
-
-/*start with ternary if i make so much freelance
-
-prompt how much pay for web design jobSalary
-
-prompt how much is the salary
-
-if salary is below but above
-
-freelance soo many jobs.
-
-conditional other than equals*/
