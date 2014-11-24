188bde4d7a101dfe9c9a72acb802b12ad6ae4b39
alcrawford <adriennelorton@hotmail.com>
2014-11-23 22:33:41 -0500

    10:33pm
    10:33pm

--- Week 3/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals_Industry/js/script.js
@@ -6,7 +6,7 @@ var codeLang = ["JS","HTML","RUBY","PHP","HTML"]
 console.log(codeLang);
 //array created and returns
 
-// In this example, I am pretending as if I am a supervisor trying to collect data on a web development job. I want to know how many projects
+// In this example, I am pretending as if I am a supervisor trying to complete a web development job. I want to know how many projects
 // require HTML. So, I create a variable with if else statements that add 1 for every instance of HTML in the array.
 
 var numHtml = 0;
@@ -45,13 +45,14 @@ var workersHtml = prompt("I am going to budget the HTML Project based on a few c
 "How many developers do you need to complete each HTML project?");
 console.log(workersHtml);
 //returns
-//Next I prompt for the time required to complete the job.
 
+//Next I prompt for the time required to complete the job.
 var hoursHtml = prompt("With a full team, how many hours will the project require for completion?");
 
 var totalOne = workersHtml * hoursHtml;
 var totalHours = 4800 / totalOne;
-//I made two variables to calculate the amount for payroll using a 4800 budget.
+//I made variables to calculate the amount for payroll using a 4800 budget.
+
 alert("You are budgeted for an hourly wage of " +totalHours+ " dollars per employee. If the wage is below satisfactory" +
 "contact me to discuss alternate arrangements.");
 //I returned the payroll amount. Check.
--- Week 3/Conditionals_Personal/js/script.js
+++ Week 3/Conditionals_Personal/js/script.js
@@ -1,3 +1,29 @@
 /*Adrienne Crawford
  Working Conditionals Personal
  WPF Week 3 Assignment*/
+
+
+
+var corpSalary = 50,000;
+
+var freeLanceOffer = prompt("How much can you pay me as a contractor for each freelance job?");
+    console.log(freeLanceOffer);
+
+var husbSays = prompt("Dear Husband, what is the minimum I can afford to bring home?");
+    console.log(husbSays);
+
+
+(freeLanceOffer >= husbSays) ? console.log("I can freelance!") : console.log("This looks corporate.");
+
+
+/*start with ternary if i make so much freelance
+
+prompt how much pay for web design jobSalary
+
+prompt how much is the salary
+
+if salary is below but above
+
+freelance soo many jobs.
+
+conditional other than equals*/
