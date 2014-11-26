4a6235a8c0fd2c9cf3aef7fc9981a4a892e03371
alcrawford <adriennelorton@hotmail.com>
2014-11-26 14:18:07 -0500

    Draft Final

--- Week 4/Functions_Industry/js/script.js
+++ Week 4/Functions_Industry/js/script.js
@@ -3,16 +3,19 @@
  WPF Week 4 Assignment
  */
 
-var Reg = prompt(How much do you pay)
-
+var Reg = prompt("In numbers only, how much do you pay to register your site every year?");
+ console.log(Reg);
+//first user variable prompt to return argument/parameter field
+var host = prompt("In numbers only, how much do you pay for yearly hosting?");
+ console.log(host);
+//second user variable prompt to return argument/parameter field
 
 siteCost(Reg,host);                    //set up argument for cost of yearly website maintenance
 
 
-
-function siteCost(Reg, host){       //parameters for yearly website registration and yearly hosting
-  var siteCost = Reg + host;        //function definition added together
-  console.log(siteCost);            //result check
+function siteCost(Reg, host){                           //parameters for yearly website registration and yearly hosting
+  var siteCost = parseInt(Reg) + parseInt(host);        //function definition added together using parsing
+  console.log(siteCost);                                //result check
 }
 
 
