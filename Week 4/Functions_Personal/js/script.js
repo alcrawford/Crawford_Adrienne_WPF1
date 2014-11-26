3871b93a434b963246b71744f1bcffc6d2b03deb
alcrawford <adriennelorton@hotmail.com>
2014-11-26 15:42:22 -0500

    draft

--- Week 4/Functions_Personal/js/script.js
+++ Week 4/Functions_Personal/js/script.js
@@ -3,11 +3,23 @@
  WPF Week 4 Assignment
  */
 
-var bookTopic = prompt("I am writing two books, one is about the scientific study of paranormal activity and the other is " +
-" about a true life drug-smuggler.In numbers, would you rather read book 1 or 2?");
- console.log(bookTopic);
+
 
 var bookLength = prompt(" In numbers, tell me if you would prefer to read a 300 or 400 page book.");
  console.log(bookLength);
 
+var pageWrite = prompt("In numbers, tell me how many pages you think a novel writer should complete daily?");
+ console.log(pageWrite);
+
 (400 <= bookLength)? console.log ("This may take more than a year to write!") : console.log("I may write this in a year!");
+
+oneYear(bookLength, pageWrite);
+
+function oneYear(pageWrite, bookLength)
+
+
+
+
+if (pageWrite => 5){
+
+}
